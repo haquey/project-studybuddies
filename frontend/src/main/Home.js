18252382011be20c372098e4494ac364c7594b2f
Yasir Haque <yasir.haque@outlook.com>
2020-03-20 14:14:27 -0400

    spelling fix

--- frontend/src/main/Home.js
+++ frontend/src/main/Home.js
@@ -5,8 +5,8 @@ import Format from './Format';
 const Home = () => {
     return (
         <Format 
-        title="Welcome to StuddyBuddy" 
-        desc="StudyBuddy is a collaborative digital notebook tool." 
+        title="Welcome to Study Buddy" 
+        desc="Study Buddy is a collaborative digital notebook tool." 
         className="container-fluid">
         </Format>
     );
