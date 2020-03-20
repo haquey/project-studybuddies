88a396118335f88199ec25f0d8286160a1bccb8e
Adnan Shahid <adnan.shahid13@hotmail.com>
2020-03-20 00:38:08 -0400

    complete notebook select with backend

--- frontend/src/main/NotebookSelect.js
+++ frontend/src/main/NotebookSelect.js
@@ -1,33 +1,92 @@
 import React, { Component } from 'react';
 import "./notebook/Styles.css";
-import { Card } from 'semantic-ui-react';
+import { Card, Button, Icon, Input } from 'semantic-ui-react';
 import { Redirect } from 'react-router';
+import { API } from "../config";
 
 class NotebookSelect extends Component {
     state = {
         redirect: null,
-        notebooks: [
-            {
-                id: "0",
-                title: "Adnan's notebook",
-                author: 'Adnan Shahid'
-            },
-            {
-                id: "1",
-                title: "Yasir's notebook",
-                author: 'Yasir Haque'
-            },
-            {
-                id: "3",
-                title: "Thierry's notebook",
-                author: 'Thierry Sans'
-            }
-        ]
+        user: JSON.parse(localStorage.getItem('jwt')).user,
+        notebooks: [],
+        isEditing: null
     }
 
-    onClick = (id) => {
+    componentDidMount() {
+        this.getNotebooks()
+    }
+
+    getNotebooks = () => {
+        fetch(`${API}/user/${this.state.user._id}/notebook/`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('jwt')).token,
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            this.setState({notebooks: data})
+        })
+        .catch(err => console.log(err));
+    }
+
+    onAddNotebook = () => {
+        let notebook = {
+            ownerId: this.state.user._id,
+            title: 'untitled',
+            collaborators: []
+        }
+        
+        fetch(`${API}/user/${this.state.user._id}/notebook/create`, {
+            method: "POST",
+            headers: {
+                Accept: 'application/json',
+                'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('jwt')).token,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(notebook)
+        })
+        .then(res => res.json())
+        .then(data => {
+            let notebooks = [...this.state.notebooks];
+            notebooks.push(data);
+            this.setState({notebooks: notebooks})
+        })
+    }
+
+    editName = (id) => {
+        this.setState({isEditing: id})
+    }
+
+    setName = (notebook) => {
+        this.setState({isEditing: null})
+
+        fetch(`${API}/user/${this.state.user._id}/notebook/${notebook._id}`, {
+            method: "PATCH",
+            headers: {
+                Accept: 'application/json',
+                'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('jwt')).token,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(notebook)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+
+    onClick = (title, id) => {
         // go to correct page for notebook id
-        this.setState({redirect: '1'})
+        this.getNotebooks()
+        // this.setState({redirect: '1'})
+    }
+
+    onChangeHandler = (e, index) => {
+        let notebooks = [...this.state.notebooks]
+        notebooks[index].title = e.target.value;
+        this.setState({notebooks: notebooks})
     }
 
     render() {
@@ -37,10 +96,27 @@ class NotebookSelect extends Component {
 
         let subjects = this.state.notebooks.map((notebook, index) => {
             return (
-                <Card key={notebook.id} style={{width: "100%"}} onClick={this.onClick}>
+                <Card key={index} style={{width: "100%"}} onClick={this.onClick}>
                     <Card.Content>
-                        <Card.Header content={notebook.title} className={notebook.author}/>
-                        <Card.Meta className="notebookAuthor" content={notebook.author} />
+                        <Card.Header>
+                            {
+                                this.state.isEditing === notebook._id ?
+                                <Input
+                                    value={this.state.notebooks[index].title}
+                                    onChange={(e) => this.onChangeHandler(e, index)}
+                                    autoFocus
+                                    onBlur={() => this.setName(notebook)}
+                                />
+                            :
+                                <div className="selectHeader">
+                                    <p style={{margin: 0}}>{notebook.title}</p>
+                                    <Button style={{marginLeft: "auto"}} onClick={() => this.editName(notebook._id)}>
+                                        <Icon style={{margin: 0}} name='edit'/>
+                                    </Button>
+                                </div>
+                            }
+                        </Card.Header>
+                        {/* <Card.Meta className="notebookAuthor" content={notebook.author} /> */}
                         {/* <Card.Description content='Jake is a drummer living in New York.' /> */}
                     </Card.Content>
                 </Card>
@@ -52,7 +128,14 @@ class NotebookSelect extends Component {
                 <div className="notebookSelectContainer">
                     <Card style={{width: '100%'}}>
                         <Card.Content>
-                            <Card.Header content="Select or Add a Notebook!" className="selectHeader"/>
+                            <Card.Header>
+                                <div className="selectHeader">
+                                    <p style={{margin: 0}}>Add or Select a Notebook!</p>
+                                    <Button style={{marginLeft: "auto"}} onClick={this.onAddNotebook}>
+                                        <Icon style={{margin: 0}} name='plus'/>
+                                    </Button>
+                                </div>
+                            </Card.Header>
                                 {subjects}
                         </Card.Content>
                     </Card>
