96e6859acb4ac9f91012228e97fd10f93551f584
Adnan Shahid <adnan.shahid13@hotmail.com>
2020-04-12 14:18:45 -0400

    add credit link

--- frontend/src/main/Home.js
+++ frontend/src/main/Home.js
@@ -1,14 +1,21 @@
 
 import React from 'react';
 import Format from './Format';
+import { Link, withRouter } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <Format 
-        title="Welcome to Study Buddy" 
-        desc="Study Buddy is a collaborative digital notebook tool." 
-        className="container-fluid">
-        </Format>
+        <div>
+            <Format 
+                title="Welcome to Study Buddy" 
+                desc="Study Buddy is a collaborative digital notebook tool." 
+                className="container-fluid">
+            </Format>
+            <div>
+                <Link className="nav-link" to="/credits">Credits</Link>
+            </div>
+        </div>
+        
     );
 };
 export default Home;
